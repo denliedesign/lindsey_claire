ecc3bfc6ac19c1156cb80f0cde51ad1e8b97c275
denliedesign <customdenlie@gmail.com>
2024-03-05 10:26:33 -0600

    summer 24

--- resources/views/crystal-springs.blade.php
+++ resources/views/crystal-springs.blade.php
@@ -14,22 +14,40 @@
         <p class="text-center my-3">
             <a href="mailto:crystalsprings@lcdanceco.com">crystalsprings@lcdanceco.com</a>
         </p>
+        <div class="d-flex justify-content-center my-4">
+            <a href="https://app.thestudiodirector.com/lindseyclairedancecomp/portal.sd?page=Login" target="_blank" class="btn-opacity"><div class="mx-3 shadow btn btn-lg btn-red btn-family">Register</div></a>
+            <a href="/images/cs/summer-cs.pdf" target="_blank" class="btn-opacity"><div class="shadow btn btn-lg btn-red btn-family mx-3">View PDF</div></a>
+        </div>
+        <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-2">
+            <div class="col"><img src="/images/cs/summer-cs-1.jpg" alt="summer graphic" class="img-fluid my-3 rounded"></div>
+            <div class="col"><img src="/images/cs/summer-cs-2.jpg" alt="summer graphic" class="img-fluid my-3 rounded"></div>
+            <div class="col"><img src="/images/cs/summer-cs-3.jpg" alt="summer graphic" class="img-fluid my-3 rounded"></div>
+            <div class="col"><img src="/images/cs/summer-cs-4.jpg" alt="summer graphic" class="img-fluid my-3 rounded"></div>
+            <div class="col"><img src="/images/cs/summer-cs-5.jpg" alt="summer graphic" class="img-fluid my-3 rounded"></div>
+            <div class="col"><img src="/images/cs/summer-cs-6.jpg" alt="summer graphic" class="img-fluid my-3 rounded"></div>
+        </div>
+
+
 {{--        <p class="text-center">--}}
 {{--            Come one, Come all to the greatest week of summer! Wanting to improve your tumbling technique for cheer or gymnastics, or just want to learn something new? Our Circus camp is perfect for learning the basics in tumble & cheer! This camp is for ages 2-6 year olds June 20-24.--}}
 {{--            <br><br>--}}
 {{--            Our first summer camp announced for our Crystal Springs location! You can sign up online today through the link below. We will be doing our first summer camp in June with a performance at the Tomato festival Friday of camp week!--}}
 {{--        </p>--}}
-        <div class="d-flex justify-content-center my-4">
-            <a href="https://app.thestudiodirector.com/lindseyclairedancecomp/portal.sd?page=Login" target="_blank" class="btn-opacity"><div class="shadow btn btn-lg btn-red btn-family">Sign Up Today!</div></a>
-        </div>
-        <div class="my-3">
-            <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-2">
-                <img src="/images/2_1.jpg" alt="classes" class="img-fluid my-1">
-                <img src="/images/3_1.jpg" alt="classes" class="img-fluid my-1">
-                <img src="/images/4_1.jpg" alt="classes" class="img-fluid my-1">
-                <img src="/images/5_1.jpg" alt="classes" class="img-fluid my-1">
-            </div>
-        </div>
+
+        <!-- old fall schedule content -->
+{{--        <div class="d-flex justify-content-center my-4">--}}
+{{--            <a href="https://app.thestudiodirector.com/lindseyclairedancecomp/portal.sd?page=Login" target="_blank" class="btn-opacity"><div class="shadow btn btn-lg btn-red btn-family">Sign Up Today!</div></a>--}}
+{{--        </div>--}}
+{{--        <div class="my-3">--}}
+{{--            <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-2">--}}
+{{--                <img src="/images/2_1.jpg" alt="classes" class="img-fluid my-1">--}}
+{{--                <img src="/images/3_1.jpg" alt="classes" class="img-fluid my-1">--}}
+{{--                <img src="/images/4_1.jpg" alt="classes" class="img-fluid my-1">--}}
+{{--                <img src="/images/5_1.jpg" alt="classes" class="img-fluid my-1">--}}
+{{--            </div>--}}
+{{--        </div>--}}
+        <!-- end old fall schedule content -->
+
 {{--        <div class="row">--}}
 {{--            <div class="d-flex justify-content-center">--}}
 {{--                <img src="/images/crystal-springs-schedule.png" alt="crystal springs schedule" class="img-fluid">--}}
--- resources/views/summer.blade.php
+++ resources/views/summer.blade.php
@@ -6,41 +6,26 @@
     </div>
 
     <div class="container">
-        <h1 class="text-center my-5">Summer</h1>
+        <h1 class="text-center mt-5">Summer 2024</h1>
 {{--        <p class="text-center">--}}
 {{--            <a href="/images/calendar.pdf" target="_blank">View Our Full Calendar</a>--}}
 {{--        </p>--}}
         <div class="d-flex justify-content-center my-4">
-            <a href="https://app.thestudiodirector.com/lindseyclairedancecomp/portal.sd?page=Login" target="_blank" class="btn-opacity"><div class="shadow btn btn-lg btn-red btn-family">Register</div></a>
+            <a href="https://app.thestudiodirector.com/lindseyclairedancecomp/portal.sd?page=Login" target="_blank" class="btn-opacity"><div class="mx-3 shadow btn btn-lg btn-red btn-family">Register</div></a>
+            <a href="/images/byram/summer-byram.pdf" target="_blank" class="btn-opacity"><div class="shadow btn btn-lg btn-red btn-family mx-3">View PDF</div></a>
         </div>
-        <div class="row">
-            <div class="col-sm">
-                <img src="/images/summer-camps-2023.jpg" alt="summer camps" class="img-fluid">
-            </div>
-            <div class="col-sm">
-                <img src="/images/summer-schedule-2023b.jpg" alt="summer schedule" class="img-fluid">
-            </div>
-        </div>
-        <div class="row my-4">
-            <div class="col-sm-3"></div>
-            <div class="col-sm">
-                <img src="/images/summer-info-2023.jpg" alt="summer info" class="img-fluid">
-            </div>
-            <div class="col-sm-3"></div>
-{{--            <div class="col-sm">--}}
-{{--                <img src="/images/fall-registration.png" alt="fall registration" class="img-fluid">--}}
-{{--            </div>--}}
-        </div>
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4">
-            <img src="/images/summer-frozen-1.jpg" alt="summer info" class="img-fluid my-2">
-            <img src="/images/summer-frozen-2.jpg" alt="summer info" class="img-fluid my-2">
-            <img src="/images/summer-tumble-1.jpeg" alt="summer info" class="img-fluid my-2">
-            <img src="/images/summer-tumble-2.jpg" alt="summer info" class="img-fluid my-2">
-            <img src="/images/summer-dance.jpg" alt="summer info" class="img-fluid my-2">
-            <img src="/images/summer-ballet.jpeg" alt="summer info" class="img-fluid my-2">
-            <img src="/images/summer-princess.jpeg" alt="summer info" class="img-fluid my-2">
-            <img src="/images/summer-musical.jpeg" alt="summer info" class="img-fluid my-2">
-
+        <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-2">
+            <div class="col"><img src="/images/byram/summer-byram-1.jpg" alt="summer graphic" class="img-fluid my-3 rounded"></div>
+            <div class="col"><img src="/images/byram/summer-byram-2.jpg" alt="summer graphic" class="img-fluid my-3 rounded"></div>
+            <div class="col"><img src="/images/byram/summer-byram-3.jpg" alt="summer graphic" class="img-fluid my-3 rounded"></div>
+            <div class="col"><img src="/images/byram/summer-byram-4.jpg" alt="summer graphic" class="img-fluid my-3 rounded"></div>
+            <div class="col"><img src="/images/byram/summer-byram-5.jpg" alt="summer graphic" class="img-fluid my-3 rounded"></div>
+            <div class="col"><img src="/images/byram/summer-byram-6.jpg" alt="summer graphic" class="img-fluid my-3 rounded"></div>
+            <div class="col"><img src="/images/byram/summer-byram-7.jpg" alt="summer graphic" class="img-fluid my-3 rounded"></div>
+            <div class="col"><img src="/images/byram/summer-byram-8.jpg" alt="summer graphic" class="img-fluid my-3 rounded"></div>
+            <div class="col"><img src="/images/byram/summer-byram-9.jpg" alt="summer graphic" class="img-fluid my-3 rounded"></div>
+            <div class="col"><img src="/images/byram/summer-byram-10.jpg" alt="summer graphic" class="img-fluid my-3 rounded"></div>
+            <div class="col"><img src="/images/byram/summer-byram-11.jpg" alt="summer graphic" class="img-fluid my-3 rounded"></div>
         </div>
     </div>
 
